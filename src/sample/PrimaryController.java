c974988166779bd01db533c86fc4c22f758ed327
Jovalb <jovorapper1@gmail.com>
2019-10-08 22:13:20 +0200

    oppdatert programmet litt, gjort at det er mer ryddig i listen, men sliter med å finne en teknikke for å sortere listen etter hver rotatsjon uten at den tar med seg alle tallene i arrayet fra før. Må nok kopierer til et midlertidig array for å og sortere/rotere

--- src/sample/PrimaryController.java
+++ src/sample/PrimaryController.java
@@ -1,16 +1,20 @@
 package sample;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.LinkedList;
+import java.lang.reflect.Array;
+import java.util.*;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import sun.security.util.ArrayUtil;
+
+import javax.swing.*;
 
 public class PrimaryController {
 
@@ -32,7 +36,7 @@ public class PrimaryController {
         return b == 0 ? a : gcd(b, a % b);
     }
 
-    public static void rotasjon(int [] a, int k){
+    public static void rotasjon(int[] a, int k) {
         int n = a.length;
         if (n < 2) return;         // ingen rotasjon
         if ((k %= n) < 0) k += n;                     // motsatt vei?
@@ -54,11 +58,32 @@ public class PrimaryController {
         }
     }
 
-    public static String arrayToString(int [] a){
+    void sort(int arr[])
+    {
+        int n = arr.length;
+
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n-1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+
+            // Swap the found minimum element with the first
+            // element
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static String arrayToString(int[] a) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append('[');
 
-        for (int i = 0; i < a.length-1; i++) {
+        for (int i = 0; i < a.length - 1; i++) {
             stringBuilder.append(a[i]).append(", ");
         }
         stringBuilder.setLength(stringBuilder.length() - 2);
@@ -67,29 +92,56 @@ public class PrimaryController {
         return stringBuilder.toString();
     }
 
+
     @FXML
-    private void test (){
-        String [] input = inputBox.getText().split("-");
-        if (input.length < 8){
-            Alert error = new Alert(Alert.AlertType.ERROR,"Du har skrevet inn for få tall!");
+    private void test() {
+        String[] input = inputBox.getText().split("-");
+        Boolean inneholderUgyldigTall = false;
+
+        if (input.length < 8) {
+            Alert error = new Alert(Alert.AlertType.ERROR, "Du har skrevet inn for få tall!");
             error.showAndWait();
-        } else if (input.length > 8){
-            Alert error = new Alert(Alert.AlertType.ERROR,"Du har skrevet inn for få tall!");
+        } else if (input.length > 8) {
+            Alert error = new Alert(Alert.AlertType.ERROR, "Du har skrevet inn for mange tall!");
             error.showAndWait();
         } else {
-            int [] listeArray = new int [input.length];
 
+            int[] listeArray = new int[input.length];
             for (int i = 0; i < input.length; i++) {
                 listeArray[i] = Integer.parseInt(input[i]);
+                if (listeArray[i] < 0 || listeArray[i] > 34) {
+                    inneholderUgyldigTall = true;
+                }
             }
-            observableList.add(arrayToString(listeArray));
-            //maks 8 rekker for 8 tall
-            for (int i = 0; i < listeArray.length-1; i++) {
-                rotasjon(listeArray,1);
-                System.out.println(arrayToString(listeArray));
+
+            Arrays.sort(listeArray);
+
+
+            Set<Integer> lagring = new HashSet<>();
+
+            for (int i : listeArray) {
+                if (lagring.add(i) == false) {
+                    inneholderUgyldigTall = true;
+                }
+            }
+
+
+
+            if (inneholderUgyldigTall) {
+                Alert error = new Alert(Alert.AlertType.ERROR, "Noen av tallene dine er feil eller duplikat, dobbeltsjekk! ");
+                error.showAndWait();
+            } else {
                 observableList.add(arrayToString(listeArray));
+                //maks 8 rekker for 8 tall
+                for (int i = 0; i < listeArray.length - 1; i++) {
+                    rotasjon(listeArray, 1);
+                    System.out.println(arrayToString(listeArray));
+                    observableList.add(arrayToString(listeArray));
+                }
+                SortedList<ObservableList> sortedList = new SortedList(observableList);
+                listeView.setItems(observableList);
             }
-            listeView.setItems(observableList);
+
         }
 
     }
